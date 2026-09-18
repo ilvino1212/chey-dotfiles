6c8a647c84f413db721165507b0f3b22c8980ea2
ilvino1212 <cheyennearoldo2008@gmail.com>
2026-09-18 21:51:26 +0200

    sync current Hyprland and UWSM setup

---
+++ home/.config/hypr/config/monitors.lua
@@ -0,0 +1,15 @@
+-- Monitor wiki https://wiki.hypr.land/Configuring/Basics/Monitors/
+-- Example: output can be found with hyprctl monitors. Edit variables.lua for the monitor outputs instead of here directly
+-- hl.monitor({
+--     output    = MONITOR1,
+--     mode      = "1920x1080@170",
+--     position  = "0x0",
+--     scale     = "1",
+-- })
+
+hl.monitor({
+    output    = MONITOR1,
+    mode      = "1920x1080@170Hz",
+    position  = "auto",
+    scale     = "auto",
+})
